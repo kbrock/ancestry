f437772f01f5d9f434252691011668a5226745b3
Keenan Brock <keenan@thebrocks.net>
2016-12-04 09:29:52 -0500

    Use connection_config to detect db
    Goal is to inject behavior at include time instead of scope runtime

--- lib/ancestry/has_ancestry.rb
+++ lib/ancestry/has_ancestry.rb
@@ -45,7 +45,7 @@ module Ancestry
       scope :subtree_of, lambda { |object| where(subtree_conditions(object)) }
       scope :siblings_of, lambda { |object| where(sibling_conditions(object)) }
       scope :ordered_by_ancestry, Proc.new { |order|
-        if %w(mysql mysql2 sqlite sqlite3 postgresql).include?(connection.adapter_name.downcase) && ActiveRecord::VERSION::MAJOR >= 5
+        if %w(mysql mysql2 sqlite sqlite3 postgresql).include?(connection_config[:adapter].downcase) && ActiveRecord::VERSION::MAJOR >= 5
           reorder(
             Arel::Nodes::Ascending.new(Arel::Nodes::NamedFunction.new('COALESCE', [arel_table[ancestry_column], Arel.sql("''")])),
             order
